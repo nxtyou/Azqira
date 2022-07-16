ac3dd2ee81ac9723cb8a3571af103b044fd4e12a
evrrnv <github.y9fqc@leave.email>
2022-07-16 20:31:38 +0100

    move from vue to vanilla js & devices section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,20 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        aktiv: ["Aktiv Grotesk Corp", "sans-serif"],
-      },
-      colors: {
-        black: "#202028",
-        zeus: "#17120E",
-        pampas: "#F2EFE8",
-        "cold-gray": "#181818",
-        shark: "#1F2123",
-        shamrock: "#27D17E",
-        silver: "#C9C9C9",
+    content: ["./public/**/*.html"],
+    theme: {
+      extend: {
+        fontFamily: {
+          aktiv: ["Aktiv Grotesk Corp", "sans-serif"],
+        },
+        colors: {
+          black: "#202028",
+          zeus: "#17120E",
+          pampas: "#F2EFE8",
+          "cold-gray": "#181818",
+          shark: "#1F2123",
+          shamrock: "#27D17E",
+          silver: "#C9C9C9",
+        },
       },
     },
-  },
-  plugins: [],
-};
+    plugins: [],
+  };
