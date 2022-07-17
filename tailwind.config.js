327dbb90ab6d335eb8bfc647becbce160c945f4c
evrrnv <github.y9fqc@leave.email>
2022-07-17 22:59:03 +0100

    How it works section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,32 @@
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
     content: ["./public/**/*.html"],
     theme: {
-      extend: {
-        fontFamily: {
-          aktiv: ["Aktiv Grotesk Corp", "sans-serif"],
+        extend: {
+            fontFamily: {
+                aktiv: ["Aktiv Grotesk Corp", "sans-serif"],
+            },
+            colors: {
+                black: "#202028",
+                zeus: "#17120E",
+                "cold-gray": "#181818",
+                shark: "#1F2123",
+                shamrock: "#27D17E",
+                silver: "#C9C9C9",
+                "mine-shaft": "#383838",
+                boulder: "#747474",
+                pampas: "#F2EFE8",
+                karaka: "#24160B",
+                tundora: "#494949",
+            }
         },
-        colors: {
-          black: "#202028",
-          zeus: "#17120E",
-          pampas: "#F2EFE8",
-          "cold-gray": "#181818",
-          shark: "#1F2123",
-          shamrock: "#27D17E",
-          silver: "#C9C9C9",
-          "mine-shaft": "#383838",
-          boulder: "#747474",
-          pampas: "#F2EFE8",
-          karaka: "#24160B"
-        },
-      },
     },
-    plugins: [],
-  };
+    plugins: [
+        require("tailwindcss"),
+        require("autoprefixer"),
+        plugin(function ({ addVariant }) {
+            addVariant("active-tab", ".active-tab &");
+        }),
+    ],
+};
